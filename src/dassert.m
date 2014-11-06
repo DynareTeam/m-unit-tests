a965878fad6e9fc8e59830cb1d133843983e1091
Stéphane Adjemian <stepan@telemachus.lan.cepremap.org>
2014-11-06 14:21:20 +0100

    Updated header.

--- src/dassert.m
+++ src/dassert.m
@@ -1,24 +1,23 @@
-function t = dyn_assert(A,B,tol)
+function t = dassert(A,B,tol)
+
 % This function tests the equality of two objects.
 
-% Copyright (C) 2011-2012 Dynare Team
+% Copyright (C) 2011-2014 Dynare Team
 %
-% This file is part of Dynare.
+% This file is part of Dynare (m-unit-tests module).
 %
 % Dynare is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation, either version 3 of the License, or
 % (at your option) any later version.
 %
-% Dynare is distributed in the hope that it will be useful,
-% but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% GNU General Public License for more details.
+% Dynare's m-unit-tests module is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+% or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for 
+% more details.
 %
 % You should have received a copy of the GNU General Public License
 % along with Dynare.  If not, see <http://www.gnu.org/licenses/>.
-
-% Original author: stephane DOT adjemian AT univ DASH lemans DOT fr
 
 if ( (nargin<3) || isempty(tol) )
     use_isequal_matlab_builtin = 1;
