a9fc113f0b2cc214af7fe68a2684b3e8ee5f1687
Stéphane Adjemian (Hermes) <stephane.adjemian@univ-lemans.fr>
2015-11-17 11:51:47 +0100

    Added skipline as a missing function.

--- src/initialize_unit_tests_toolbox.m
+++ src/initialize_unit_tests_toolbox.m
@@ -4,3 +4,7 @@ unit_tests_src_root = strrep(which('initialize_unit_tests_toolbox'),'initialize_
 if ~exist('isoctave','file') 
     addpath([unit_tests_src_root '/missing/isoctave'])
 end
+
+if ~exist('skipline','file')
+    addpath([unit_tests_src_root '/missing/skipline'])
+end
